bd4a5009ee2076975e6c839c071b8adede182209
juanpcs <juanpacarrillo1@gmail.com>
2025-05-07 02:19:10 -0600

    [Add] Nuevos scripts para detección de ojos y boca y para pruebas de los nuevos modelos

---
+++ eye-mouth-detection.py
@@ -0,0 +1,114 @@
+import cv2
+import mediapipe as mp
+import time
+import os
+
+#-----------------------------------------------------------------------------------#
+
+##### Script para probar la detección y captura de rostros utilizando Mediapipe #####
+
+#-----------------------------------------------------------------------------------#
+
+# Se inicializa la cámara
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # fijar el ancho a 640 píxeles
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # fijar el alto a 480 píxeles
+
+# Se inicializa mediapipe para la detección de caras
+mp_face_detection = mp.solutions.face_detection
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.9)
+mp_drawing = mp.solutions.drawing_utils
+
+# Se crea un directorio para guardar las fotos si no existe
+output_dir = "Eyes"
+os.makedirs(output_dir, exist_ok=True)
+# Se crea un directorio para guardar las fotos si no existe
+output_dir2 = "Mouths"
+os.makedirs(output_dir2, exist_ok=True)
+
+img_counter = 0
+start_time = time.time()
+# Se define el tamaño estándar para las imágenes recortadas
+standard_size = (64, 64)  
+    
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: No se pudo capturar la imagen")
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = face_detection.process(frame_rgb)
+
+    if results.detections is not None:
+        for detection in results.detections:
+
+            #Se obtienen las coordenadas de la cara detectada en el frame
+            y_min = int(detection.location_data.relative_bounding_box.ymin * 480)
+            height = int(detection.location_data.relative_bounding_box.height * 480)
+            x_min = int(detection.location_data.relative_bounding_box.xmin * 640)
+            width = int(detection.location_data.relative_bounding_box.width * 640)
+
+            x_RE = int(detection.location_data.relative_keypoints[0].x * 640)
+            y_RE = int(detection.location_data.relative_keypoints[0].y * 480)
+
+            x_RET = int(detection.location_data.relative_keypoints[4].x * 640)
+            y_RET = int(detection.location_data.relative_keypoints[4].y * 480)
+
+            x_MC = int(detection.location_data.relative_keypoints[3].x * 640)
+            y_MC = int(detection.location_data.relative_keypoints[3].y * 480)
+
+            x_NT = int(detection.location_data.relative_keypoints[2].x * 640)
+            y_NT = int(detection.location_data.relative_keypoints[2].y * 480)
+
+            # Se recorta la imagen para obtener solo las regiones del ojo derecho y de la boca
+            cropped_eye = frame[y_min:y_NT-15, x_RET+10:x_NT-10]
+            cropped_mouth = frame[y_NT:y_min+height-15, x_min+20:x_min+width-20]
+
+            # Se redimensionan las imágenes recortadas al tamaño estándar
+            resized_eye = cv2.resize(cropped_eye, standard_size)
+            resized_mouth = cv2.resize(cropped_mouth, standard_size)
+
+            # Se convierten escala de grises
+            gray_eye = cv2.cvtColor(resized_eye, cv2.COLOR_BGR2GRAY)
+            gray_mouth = cv2.cvtColor(resized_mouth, cv2.COLOR_BGR2GRAY)
+
+            # Guarda la imagen resultante
+            cv2.imwrite('eye_bn.jpg', gray_eye)
+            cv2.imwrite('mouth_bn.jpg', gray_mouth)
+
+            # Se dibuja el bounding box del ojo derecho en la imagen
+            cv2.rectangle(frame, (x_RET, y_min), (x_NT, y_NT-15), (0, 255, 0), 2)
+
+            # Se dibuja el bounding box de la boca en la imagen
+            cv2.rectangle(frame, (x_min+20, y_NT), (x_min+width-20, y_min+height-15), (0, 255, 0), 2)
+
+            current_time = time.time()
+
+            # Se verifica si han pasado x segundos desde la última captura
+            if current_time - start_time >= 0.5:
+                # Se guardan las imágenes recortadas
+                img_name = f"ojo_stndr_{img_counter}.png"
+                img_name2 = f"boca_stndr_{img_counter}.png"
+
+                cv2.imwrite(os.path.join(output_dir, img_name), gray_eye)
+                cv2.imwrite(os.path.join(output_dir2, img_name2), gray_mouth)
+
+                print(f"Imagen estandarizada guardada como '{img_name}'")
+                print(f"Imagen estandarizada guardada como '{img_name2}'")
+                img_counter += 1
+                        
+                start_time = current_time
+
+    # Se muestra la imagen con las anotaciones en la ventana principal
+    cv2.imshow('Sistema de deteccion de fatiga para conductores', frame) 
+
+    # Se sale del bucle si se presiona la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
